d6e3b55ac7021bf423b5a22b543a3c5ba890fbdd
james yu <jhyu0210@gmail.com>
2018-02-11 11:39:58 +0900

    initial commit

---
+++ src/main/java/com/joyclad/LecSlippApplication.java
@@ -0,0 +1,12 @@
+package com.joyclad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecSlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecSlippApplication.class, args);
+	}
+}
